4f12694ca209c2275ac77fd667ee4bc9d29e5ce6
bhsohn <hanmuson1@gmail.com>
2025-09-04 15:03:42 +0900

    파티클 정보 추가

--- Survival/Assets/00_Scripts/Main/P_Handler.cs
+++ Survival/Assets/00_Scripts/Main/P_Handler.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class P_Handler : MonoBehaviour
 {
     public static M_Object m_Object = null;
+    [SerializeField] private GameObject HitParticle;
 
     public void Hit()
     {
         m_Object.HP -= 20;
+
+        Vector3 pos = new Vector3(
+            m_Object.transform.position.x + Random.Range(-0.5f, 0.5f),
+            m_Object.transform.position.y + 1.5f,
+            m_Object.transform.position.z + Random.Range(-0.5f, 0.5f));
+
+        Instantiate(HitParticle, pos, Quaternion.identity);
         m_Object.HP_Init();
     }
-    
+
 }
