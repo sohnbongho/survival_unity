789e44883f2e58d2cebb5c0d55dcc7055dddf4c2
bhsohn <hanmuson1@gmail.com>
2025-09-11 18:01:22 +0900

    오브젝트 생성 처리

--- Survival/Assets/00_Scripts/UI/Navagation_Mng.cs
+++ Survival/Assets/00_Scripts/UI/Navagation_Mng.cs
@@ -1,5 +1,6 @@
-using UnityEditor.Localization.Plugins.XLIFF.V12;
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Navagation_Mng : MonoBehaviour
 {
@@ -11,25 +12,37 @@ public class Navagation_Mng : MonoBehaviour
     }
 
     [SerializeField] private Transform Content;
+    [SerializeField] private int Maximum;
     Nav_Item[] P_Item;
-    Nav_Item Toast_Item;
 
     private void Start()
     {
-        P_Item = GetComponentsInChildren<Nav_Item>(true);        
+        P_Item = GetComponentsInChildren<Nav_Item>(true);
     }
 
     public void PanelGet_Item(Item_Scriptable data, int count)
     {
-        var go = Instantiate(P_Item[0], Content);
-        go.gameObject.SetActive(true);
+        var go = MakeItem(0);
         go.Init(data, count);
     }
     public void PanelGet_Toast(Scriptable_Base data, string key)
     {
-        var go = Instantiate(P_Item[1], Content);
-        go.gameObject.SetActive(true);
+        var go = MakeItem(1);
         go.Init_Building(data, key);
+    }
+
+    private Nav_Item MakeItem(int value)
+    {
+        var go = Instantiate(P_Item[value], Content);
+        go.transform.SetAsFirstSibling(); // 맨 처음 위치로 생성해라
+        go.gameObject.SetActive(true);
+
+        if (Content.childCount > Maximum)
+        {
+            // 즉시 오브젝트를 파괴하라는 함수
+            DestroyImmediate(Content.GetChild(Content.childCount - 1).gameObject);
+        }
+        return go;
     }
 
 }
